ff6f923a8643b6d83aae56186ef3791ded533b73
Marcin Putra <marcin@putra.pro>
2024-04-18 16:08:41 +0200

    A child's play: 100%

--- solo/puzzles/easy/AChildsPlay.cpp
+++ solo/puzzles/easy/AChildsPlay.cpp
@@ -15,6 +15,11 @@ bool operator==(const Position& lhs, const Position& rhs)
    return lhs.x == rhs.x && lhs.y == rhs.y;
 }
 
+bool operator==(const std::pair<Position, Position>& lhs, const std::pair<Position, Position>& rhs)
+{
+   return lhs.first == rhs.first && lhs.second == rhs.second;
+}
+
 Position operator+(const Position& lhs, const Position& rhs)
 {
    return { lhs.x + rhs.x, lhs.y + rhs.y, lhs.moves + rhs.moves };
@@ -43,42 +48,34 @@ int main()
       }
    }
 
-   std::vector<Position> history;
+   std::vector<std::pair<Position, Position>> history;
 
    Position dir = { 0, -1, 1 };
    while (current_pos.moves < n)
    {
-      auto find = std::find(history.begin(), history.end(), current_pos);
+      auto find = std::find(history.begin(), history.end(), std::pair<Position, Position>(current_pos, dir));
       if (find != history.end())
       {
-         n = (n - find->moves) % (current_pos.moves - find->moves);
-         current_pos.moves = find->moves;
+         n = (n - find->first.moves) % (current_pos.moves - find->first.moves);
+         current_pos.moves = 0;
          history.clear();
       }
-      history.push_back(current_pos);
+      history.push_back({ current_pos, dir });
       Position new_pos = current_pos + dir;
       if (map[new_pos.y][new_pos.x] == '#')
       {
-         if (dir.x != 0)
-         {
-            dir.y = dir.x;
-            dir.x = 0;
-         }
-         else if (dir.y != 0)
-         {
-            dir.x = -dir.y;
-            dir.y = 0;
-         }
+         std::swap(dir.x, dir.y);
+         dir.x = -dir.x;
       }
       map[current_pos.y][current_pos.x] = '.';
+      new_pos = current_pos + dir;
+      if (map[new_pos.y][new_pos.x] == '#')
+      {
+         std::swap(dir.x, dir.y);
+         dir.x = -dir.x;
+      }
       current_pos = current_pos + dir;
       map[current_pos.y][current_pos.x] = 'O';
-
-      for (const auto& row : map)
-      {
-         std::cerr << row << std::endl;
-      }
-      std::cerr << std::endl;
    }
 
    std::cout << current_pos.x << " " << current_pos.y << std::endl;
