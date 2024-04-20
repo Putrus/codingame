36b09b4154d5e0c84ff8d1c6566b15a66274a786
Marcin Putra <marcin@putra.pro>
2024-04-20 22:57:55 +0200

    The Labyrinth: Code progress

--- solo/puzzles/hard/TheLabyrinth.cpp
+++ solo/puzzles/hard/TheLabyrinth.cpp
@@ -1,7 +1,106 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <unordered_set>
+
+enum class Move : int
+{
+   Start = 0,
+   Up,
+   Down,
+   Left,
+   Right
+};
+
+std::ostream& operator<<(std::ostream& os, const Move& move)
+{
+   switch (move)
+   {
+      case Move::Start:
+      {
+         os << "START";
+         break;
+      }
+      case Move::Up:
+      {
+         os << "UP";
+         break;
+      }
+      case Move::Down:
+      {
+         os << "DOWN";
+         break;
+      }
+      case Move::Left:
+      {
+         os << "LEFT";
+         break;
+      }
+      case Move::Right:
+      {
+         os << "RIGHT";
+         break;
+      }
+      default:
+      {
+         os << "WRONG";
+         break;
+      }
+   }
+   return os;
+}
+
+struct Position
+{
+   int x = 0;
+   int y = 0;
+   int move = 0;
+};
+
+std::ostream& operator<<(std::ostream& os, const Position& position)
+{
+   os << position.x << ' ' << position.y << ' ' << static_cast<Move>(position.move);
+   return os;
+}
+
+std::istream& operator>>(std::istream& is, Position& position)
+{
+   is >> position.x >> position.y;
+   return is;
+}
+
+bool operator==(const Position& lhs, const Position& rhs)
+{
+   return lhs.x == rhs.x && lhs.y == rhs.y && lhs.move == rhs.move;
+}
+
+Position operator+(const Position& lhs, const Position& rhs)
+{
+   return { lhs.x + rhs.x, lhs.y + rhs.y };
+}
+
+namespace std
+{
+   template <>
+   struct hash<Position>
+   {
+      size_t operator()(const Position& position) const noexcept
+      {
+         size_t hash_value = 17;
+         hash_value = hash_value * 31 + std::hash<int>{}(position.x);
+         hash_value = hash_value * 31 + std::hash<int>{}(position.y);
+         hash_value = hash_value * 31 + std::hash<long long>{}(position.move);
+         return hash_value;
+      }
+   };
+}
+
+enum class Phase
+{
+   LookingForC,
+   GoToC,
+   GoToT
+};
 
 int main()
 {
@@ -10,7 +109,10 @@ int main()
    int a; // number of rounds between the time the alarm countdown is activated and the time the alarm goes off.
    std::cin >> r >> c >> a; std::cin.ignore();
 
-   std::string last_move = "";
+   Phase phase = Phase::LookingForC;
+   Move last_move = Move::Start;
+   std::unordered_set<Position> history;
+
    // game loop
    while (1)
    {
@@ -24,88 +126,71 @@ int main()
          std::cin >> row; std::cin.ignore();
          maze.push_back(row);
          std::cerr << row << std::endl;
+         for (int j = 0; j < row.size(); ++j)
+         {
+            if (row[j] == 'C')
+            {
+               std::cerr << "Find C in " << i << " " << j << std::endl;
+               //phase = Phase::GoToC;
+            }
+         }
       }
-
-      if (last_move == "RIGHT" || last_move == "")
+      std::cerr << "Position: " << kr << " " << kc << std::endl;
+      std::cerr << "History: " << std::endl;
+      for (const auto& pos : history)
       {
-         if (maze[kr][kc + 1] == '.' || maze[kr][kc + 1] == 'C')
-         {
-            last_move = "RIGHT";
-         }
-         else if (maze[kr + 1][kc] == '.' || maze[kr + 1][kc] == 'C')
-         {
-            last_move = "DOWN";
-         }
-         else if (maze[kr - 1][kc] == '.' || maze[kr - 1][kc] == 'C')
-         {
-            last_move = "UP";
-         }
-         else
-         {
-            last_move = "LEFT";
-         }
+         std::cerr << pos << std::endl; 
       }
 
-      if (last_move == "LEFT")
+      switch (phase)
       {
-         if (maze[kr][kc - 1] == '.' || maze[kr][kc - 1] == 'C')
+         case Phase::LookingForC:
          {
-            last_move = "LEFT";
+            if ((maze[kr][kc + 1] == '.' || maze[kr][kc + 1] == 'T' || maze[kr][kc + 1] == 'C') &&
+               history.find(Position{ kr, kc, static_cast<int>(Move::Right) }) == history.end())
+            {
+               last_move = Move::Right;
+               history.insert(Position{ kr, kc, static_cast<int>(last_move) });
+            }
+            else if ((maze[kr][kc - 1] == '.' || maze[kr][kc - 1] == 'T' || maze[kr][kc - 1] == 'C') &&
+               history.find(Position{ kr, kc, static_cast<int>(Move::Left) }) == history.end())
+            {
+               last_move = Move::Left;
+               history.insert(Position{ kr, kc, static_cast<int>(last_move) });
+            }
+            else if ((maze[kr + 1][kc] == '.' || maze[kr + 1][kc] == 'T' || maze[kr + 1][kc] == 'C') &&
+               history.find(Position{ kr, kc, static_cast<int>(Move::Down) }) == history.end())
+            {
+               last_move = Move::Down;
+               history.insert(Position{ kr, kc, static_cast<int>(last_move) });
+            }
+            else if ((maze[kr - 1][kc] == '.' || maze[kr - 1][kc] == 'T' || maze[kr - 1][kc] == 'C') &&
+               history.find(Position{ kr, kc, static_cast<int>(Move::Up) }) == history.end())
+            {
+               last_move = Move::Up;
+               history.insert(Position{ kr, kc, static_cast<int>(last_move) });
+            }
+            else
+            {
+               std::cerr << "What the fuck?" << std::endl;
+            }
+            
+            std::cout << last_move << std::endl;
+            break;
          }
-         else if (maze[kr + 1][kc] == '.' || maze[kr + 1][kc] == 'C')
+         case Phase::GoToC:
          {
-            last_move = "DOWN";
+            break;
          }
-         else if (maze[kr - 1][kc] == '.' || maze[kr - 1][kc] == 'C')
+         case Phase::GoToT:
          {
-            last_move = "UP";
+            break;
          }
-         else
+         default:
          {
-            last_move = "RIGHT";
-         }
-      }
-
-      if (last_move == "UP" || last_move == "")
-      {
-         if (maze[kr - 1][kc] == '.' || maze[kr - 1][kc] == 'C')
-         {
-            last_move = "UP";
-         }
-         else if (maze[kr][kc + 1] == '.' || maze[kr][kc + 1] == 'C')
-         {
-            last_move = "RIGHT";
-         }
-         else if (maze[kr][kc - 1] == '.' || maze[kr][kc - 1] == 'C')
-         {
-            last_move = "LEFT";
-         }
-         else
-         {
-            last_move = "DOWN";
-         }
-      }
-
-      if (last_move == "DOWN")
-      {
-         if (maze[kr + 1][kc] == '.' || maze[kr + 1][kc] == 'C')
-         {
-            last_move = "DOWN";
-         }
-         else if (maze[kr][kc + 1] == '.' || maze[kr][kc + 1] == 'C')
-         {
-            last_move = "RIGHT";
-         }
-         else if (maze[kr][kc - 1] == '.' || maze[kr][kc - 1] == 'C')
-         {
-            last_move = "LEFT";
-         }
-         else
-         {
-            last_move = "UP";
+            std::cerr << "Wrong phase" << std::endl;
+            break;
          }
       }
-
-      std::cout << last_move << std::endl; // Rick's next move (UP DOWN LEFT or RIGHT).
    }
 }
