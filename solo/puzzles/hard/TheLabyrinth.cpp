26f86db7ae59e39d3e6cfbcc1ac0e8a1b4f9f749
Marcin Putra <marcin@putra.pro>
2024-04-21 13:55:28 +0200

    The Labyrinth: 87%

--- solo/puzzles/hard/TheLabyrinth.cpp
+++ solo/puzzles/hard/TheLabyrinth.cpp
@@ -2,12 +2,12 @@
 #include <string>
 #include <vector>
 #include <unordered_set>
-#include <map>
-#include <optional>
+#include <deque>
 
 enum class Move : int
 {
-   Up = 0,
+   Start = 0,
+   Up,
    Down,
    Left,
    Right
@@ -55,12 +55,11 @@ struct Position
 {
    int x = 0;
    int y = 0;
-   int move = 0;
 };
 
 std::ostream& operator<<(std::ostream& os, const Position& position)
 {
-   os << position.x << ' ' << position.y << ' ' << static_cast<Move>(position.move);
+   os << position.x << ' ' << position.y;
    return os;
 }
 
@@ -72,7 +71,7 @@ std::istream& operator>>(std::istream& is, Position& position)
 
 bool operator==(const Position& lhs, const Position& rhs)
 {
-   return lhs.x == rhs.x && lhs.y == rhs.y && lhs.move == rhs.move;
+   return lhs.x == rhs.x && lhs.y == rhs.y;
 }
 
 Position operator+(const Position& lhs, const Position& rhs)
@@ -90,7 +89,6 @@ namespace std
          size_t hash_value = 17;
          hash_value = hash_value * 31 + std::hash<int>{}(position.x);
          hash_value = hash_value * 31 + std::hash<int>{}(position.y);
-         hash_value = hash_value * 31 + std::hash<long long>{}(position.move);
          return hash_value;
       }
    };
@@ -111,16 +109,8 @@ int main()
    std::cin >> r >> c >> a; std::cin.ignore();
 
    Phase phase = Phase::LookingForC;
-   Move last_move = Move::Right;
-   std::unordered_set<Position> history;
-
-   std::map<Move, std::vector<Move>> moves_priority =
-      { { Move::Up, { Move::Up, Move::Left, Move::Right, Move::Down } },
-      { Move::Down, { Move::Down, Move::Left, Move::Right, Move::Up } },
-      { Move::Left, { Move::Left, Move::Up, Move::Down, Move::Right } },
-      { Move::Right, { Move::Right, Move::Up, Move::Down, Move::Left } } };
-
    // game loop
+   char wanted = '?';
    while (1)
    {
       int kr; // row where Rick is located.
@@ -132,33 +122,37 @@ int main()
          std::string row; // C of the characters in '#.TC?' (i.e. one line of the ASCII maze).
          std::cin >> row; std::cin.ignore();
          maze.push_back(row);
-         std::cerr << row << std::endl;
-         for (int j = 0; j < row.size(); ++j)
-         {
-            if (row[j] == 'C')
-            {
-               std::cerr << "Find C in " << i << " " << j << std::endl;
-               //phase = Phase::GoToC;
-            }
-         }
-      }
-      std::cerr << "Position: " << kr << " " << kc << std::endl;
-      std::cerr << "History: " << std::endl;
-      for (const auto& pos : history)
-      {
-         std::cerr << pos << std::endl; 
       }
 
-      switch (phase)
+      if (maze[kr][kc] == 'C')
       {
-         case Phase::LookingForC:
+         wanted = 'T';
+      }
+
+      Move move = Move::Start;
+      while (move == Move::Start)
+      {
+         std::unordered_set<Position> history;
+         std::deque<std::pair<Position, Move>> que;
+         que.push_back({ { kc, kr }, Move::Start });
+         std::cerr << "Wanted = " << wanted << std::endl;
+         while (!que.empty())
          {
-            std::optional<Move> new_move;
-            std::vector<Move> possible_moves;
-            for (const auto& potential_next_move : moves_priority[last_move])
+            auto current_pos = que.front();
+            que.pop_front();
+
+            history.insert(current_pos.first);
+
+            if (maze[current_pos.first.y][current_pos.first.x] == wanted)
             {
-               Position new_pos = { kc, kr, 0 };
-               switch (potential_next_move)
+               move = current_pos.second;
+               break;
+            }
+
+            for (Move next_move : { Move::Up, Move::Down, Move::Left, Move::Right })
+            {
+               Position new_pos = current_pos.first;
+               switch (next_move)
                {
                   case Move::Up:
                   {
@@ -186,47 +180,28 @@ int main()
                      break;
                   }
                }
-            
-               if ((maze[new_pos.y][new_pos.x] == '.' || maze[new_pos.y][new_pos.x] == 'T' || maze[new_pos.y][new_pos.x] == 'C'))
+               if (new_pos.y >= 0 && new_pos.y < maze.size() && new_pos.x >= 0 && new_pos.x < maze[new_pos.y].size() &&
+                  maze[new_pos.y][new_pos.x] != '#' &&
+                  history.find(new_pos) == history.end())
                {
-                  possible_moves.push_back(potential_next_move);
-                  if (history.find(Position{ kr, kc, static_cast<int>(potential_next_move) }) == history.end())
+                  if (current_pos.second == Move::Start)
                   {
-                     new_move = potential_next_move;
-                     history.insert(Position{ kr, kc, static_cast<int>(last_move) });
-                     break;
-                  }  
+                     que.push_back({ new_pos, next_move });
+                  }
+                  else
+                  {
+                     que.push_back({ new_pos, current_pos.second });
+                  }
                }
             }
+         }
 
-            if (new_move)
-            {
-               last_move = new_move.value();
-            }
-            else if (!possible_moves.empty())
-            {
-               last_move = possible_moves[0];
-            }
-            else
-            {
-               std::cerr << "No possible moves" << std::endl;
-            }
-            std::cout << last_move << std::endl;
-            break;
-         }
-         case Phase::GoToC:
+         if (wanted == '?' && move == Move::Start)
          {
-            break;
-         }
-         case Phase::GoToT:
-         {
-            break;
-         }
-         default:
-         {
-            std::cerr << "Wrong phase" << std::endl;
-            break;
+            wanted = 'C';
          }
       }
+      
+      std::cout << move << std::endl;
    }
 }
