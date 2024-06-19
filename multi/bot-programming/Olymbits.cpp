99531f761fb89ef30d0605b8dedb072fbe725be3
Marcin Putra <marcin@putra.pro>
2024-06-19 14:35:25 +0200

    Olymbits: Wood 1 League

--- multi/bot-programming/Olymbits.cpp
+++ multi/bot-programming/Olymbits.cpp
@@ -4,6 +4,99 @@
 #include <algorithm>
 #include <map>
 
+struct Race
+{
+   int player_position = 0;
+   int player_stun = 0;
+   std::string racetrack;
+
+   int rateMove(const std::string& move) const
+   {
+      int score = 0;
+      if (move == "UP")
+      {
+         if (player_position + 1 == racetrack.size())
+         {
+            return score;
+         }
+         score += 1;
+         if (player_position + 2 == racetrack.size())
+         {
+            return score;
+         }
+
+         if (racetrack[player_position + 1] == '#')
+         {
+            score += 3;
+         }
+
+         if (racetrack[player_position + 2] == '#')
+         {
+            score -= 3;
+         }
+      }
+      else if (move == "LEFT")
+      {
+         score += 1;
+         if (player_position + 1 <= racetrack.size() && racetrack[player_position + 1] == '#')
+         {
+            score -= 3;
+         }
+      }
+      else if (move == "DOWN")
+      {
+         for (int i = 1; i <= 2; ++i)
+         {
+            if (player_position + i == racetrack.size())
+            {
+               break;
+            }
+            
+            ++score;
+            
+            if (racetrack[player_position + i] == '#')
+            {
+               score -= 3;
+            }
+         }
+      }
+      else
+      {
+         for (int i = 1; i <= 3; ++i)
+         {
+            if (player_position + i == racetrack.size())
+            {
+               break;
+            }
+
+            ++score;
+            
+            if (racetrack[player_position + i] == '#')
+            {
+               score -= 3;
+            }
+         }
+      }
+
+      return score;
+   }
+};
+
+int rateMove(const std::vector<Race>& races, const std::string& move)
+{
+   int score = 0;
+   for (const auto& race : races)
+   {
+      if (race.player_stun > 0)
+      {
+         continue;
+      }
+      
+      score += race.rateMove(move);
+   }
+   return score;
+}
+
 int main()
 {
    int player_idx;
@@ -11,7 +104,7 @@ int main()
    int nb_games;
    std::cin >> nb_games; std::cin.ignore();
    int player_position = 0;
-   std::string racetrack;
+   std::vector<Race> races(4);
    // game loop
    while (1)
    {
@@ -21,7 +114,7 @@ int main()
          std::string score_info;
          getline(std::cin, score_info);
       }
-      std::cerr << "nb_games = " << nb_games << std::endl;
+   
       for (int i = 0; i < nb_games; i++)
       {
          std::string gpu;
@@ -34,84 +127,63 @@ int main()
          int reg_6;
          std::cin >> gpu >> reg_0 >> reg_1 >> reg_2 >> reg_3 >> reg_4 >> reg_5 >> reg_6; std::cin.ignore();
 
-         racetrack = gpu;
-         if (player_idx == 0 && reg_3 != 0)
+         if (player_idx == 0)
          {
-            continue;
+            races[i].player_position = reg_0;
+            races[i].player_stun = reg_3;
+            races[i].racetrack = gpu;
          }
 
-         if (player_idx == 1 && reg_4 != 0)
+         if (player_idx == 1)
          {
-            continue;
+            races[i].player_position = reg_1;
+            races[i].player_stun = reg_4;
+            races[i].racetrack = gpu;
          }
 
-         if (player_idx == 2 && reg_5 != 0)
+         if (player_idx == 2)
          {
-            continue;
-         }
-         
-         int dist = 1;
-         for (int j = player_position + 1; j < racetrack.size(); ++j)
-         {
-            if (racetrack[j] == '#')
-            {
-               break;
-            }
-            ++dist;
-         }
-
-         if (dist >= 4)
-         {
-            if (distances_to_hurdles.find(4) == distances_to_hurdles.end())
-            {
-               distances_to_hurdles.insert({ 4, 1 });
-            }
-            else
-            {
-               ++distances_to_hurdles[4];
-            }
-         }
-         else
-         {
-            if (distances_to_hurdles.find(dist) == distances_to_hurdles.end())
-            {
-               distances_to_hurdles.insert({ dist, 1 });
-            }
-            else
-            {
-               ++distances_to_hurdles[dist];
-            }
-            
+            races[i].player_position = reg_2;
+            races[i].player_stun = reg_5;
+            races[i].racetrack = gpu;
          }
       }
 
-      std::pair<int, int> best = { 0, 0 };
+      int best_score = -999;
+      std::string best_move = "UNKNOWN";
 
-      std::cerr << distances_to_hurdles.size() << std::endl;
-      for (const auto& d : distances_to_hurdles)
+      int up_score = rateMove(races, "UP");
+      std::cerr << "UP: " << up_score << std::endl;
+      if (up_score > best_score)
       {
-         std::cerr << d.first << " " << d.second << std::endl;
-         if (d.second >= best.second)
-         {
-            best = d;
-         }
+         best_score = up_score;
+         best_move = "UP";
+      }
+      
+      int left_score = rateMove(races, "LEFT");
+      std::cerr << "LEFT: " << left_score << std::endl;
+      if (left_score > best_score)
+      {
+         best_score = left_score;
+         best_move = "LEFT";
       }
 
-      if (best.first == 1)
+      int down_score = rateMove(races, "DOWN");
+      std::cerr << "DOWN: " << down_score << std::endl;
+      if (down_score > best_score)
       {
-         std::cout << "UP" << std::endl;
+         best_score = down_score;
+         best_move = "DOWN";
       }
-      else if (best.first == 2)
+
+      int right_score = rateMove(races, "RIGHT");
+      std::cerr << "RIGHT: " << right_score << std::endl;
+      if (right_score > best_score)
       {
-         std::cout << "LEFT" << std::endl;
+         best_score = right_score;
+         best_move = "RIGHT";
       }
-      else if (best.first == 3)
-      {
-         std::cout << "DOWN" << std::endl;
-      }
-      else
-      {
-         std::cout << "RIGHT" << std::endl;
-      }
+
+      std::cout << best_move << std::endl;
    }
 }
