1457cadeee7746224477074b81f7a0b49e0da142
Marcin Putra <marcin@putra.pro>
2024-10-18 22:09:02 +0200

    Mountain Map: 100%

--- solo/puzzles/easy/MountainMap.cpp
+++ solo/puzzles/easy/MountainMap.cpp
@@ -20,9 +20,61 @@ int main()
       }
    }
 
-   for (int height : heights)
+   for (int i = max_height; i > 0; --i)
    {
-      
+      for (int k = 0; k < heights.size(); ++k)
+      {
+         int height = heights[k];
+         if (height < i)
+         {
+            bool higher_exist = false;
+            for (int j = k + 1; j < heights.size(); ++j)
+            {
+               if (heights[j] >= i)
+               {
+                  higher_exist = true;
+                  break;
+               }
+            }
+            if (higher_exist)
+            {
+               for (int j = 0; j < height * 2; ++j)
+               {
+                  std::cout << ' ';
+               }
+            }
+         }
+         else
+         {
+            for (int j = 0; j < height - (height - i) - 1; ++j)
+            {
+               std::cout << ' ';
+            }
+            std::cout << '/';
+            for (int j = 0; j < (height - i) * 2; ++j)
+            {
+               std::cout << ' ';
+            }
+            std::cout << '\\';
+
+            bool higher_exist = false;
+            for (int j = k + 1; j < heights.size(); ++j)
+            {
+               if (heights[j] >= i)
+               {
+                  higher_exist = true;
+                  break;
+               }
+            }
+            if (higher_exist)
+            {
+               for (int j = 0; j < height - (height - i) - 1; ++j)
+               {
+                  std::cout << ' ';
+               }
+            }
+         }
+      }
+      std::cout << std::endl;
    }
-   std::cout << "answer" << std::endl;
 }
