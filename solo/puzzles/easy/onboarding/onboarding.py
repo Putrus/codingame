44b33da179d67661f3b3c963df58091d7026b067
Marcin Putra <marcin@putra.pro>
2025-08-05 14:19:24 +0200

    Onboarding: Python 100% one-liner

--- solo/puzzles/easy/onboarding/onboarding.py
+++ solo/puzzles/easy/onboarding/onboarding.py
@@ -1,6 +1,1 @@
-while True:
-   enemy_1 = input()  # name of enemy 1
-   dist_1 = int(input())  # distance to enemy 1
-   enemy_2 = input()  # name of enemy 2
-   dist_2 = int(input())  # distance to enemy 2
-   print(enemy_1 if dist_1 < dist_2 else enemy_2)
+while True: print((lambda enemy_1, dist_1, enemy_2, dist_2: enemy_1 if dist_1 < dist_2 else enemy_2)(input(), int(input()), input(), int(input())))
