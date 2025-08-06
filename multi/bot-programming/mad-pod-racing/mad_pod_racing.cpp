630792996579f1f26399814eeac2bebb072ed017
Marcin Putra <marcin@putra.pro>
2025-08-06 23:19:10 +0200

    Mad Pod Racing: Bronze League 3184

--- multi/bot-programming/mad-pod-racing/mad_pod_racing.cpp
+++ multi/bot-programming/mad-pod-racing/mad_pod_racing.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+#include <cmath>
 
 int main()
 {
     // game loop
-    while (1)
+    while (true)
     {
         int x;
         int y;
@@ -19,17 +17,24 @@ int main()
         int opponent_y;
         std::cin >> opponent_x >> opponent_y; std::cin.ignore();
 
+        double dx = x - next_checkpoint_x;
+        double dy = y - next_checkpoint_y;
+        double distance_to_next_checkpoint = std::sqrt(dx * dx + dy * dy);
+        int destination_x = next_checkpoint_x + dx / distance_to_next_checkpoint * 400.0;
+        int destination_y = next_checkpoint_y + dy / distance_to_next_checkpoint * 400.0;
+        std::string thrust = "100";
         if (next_checkpoint_angle > 90 || next_checkpoint_angle < -90)
         {
-            std::cout << next_checkpoint_x << " " << next_checkpoint_y << " 0" << std::endl;
+            thrust = "10";        }
+        else if (next_checkpoint_angle > 45 || next_checkpoint_angle < -45)
+        {
+            thrust = "50";
         }
         else if (next_checkpoint_dist > 2000)
         {
-            std::cout << next_checkpoint_x << " " << next_checkpoint_y << " BOOST" << std::endl;
+            thrust = "BOOST";
         }
-        else
-        {
-            std::cout << next_checkpoint_x << " " << next_checkpoint_y << " 100" << std::endl;
-        }
+
+        std::cout << destination_x << " " << destination_y << " " << thrust << std::endl;
     }
 }
