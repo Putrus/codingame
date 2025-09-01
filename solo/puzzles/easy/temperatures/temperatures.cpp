6ce17c81f5d9388cfe15a3510e852ba9e68b3286
Marcin Putra <marcin@putra.pro>
2025-09-01 14:34:50 +0200

    Temperatures: C++ Featured Puzzle Event

--- solo/puzzles/easy/temperatures/temperatures.cpp
+++ solo/puzzles/easy/temperatures/temperatures.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <string>
+#include <limits>
 
 int main()
 {
-   int closest = 99999;
-   int n; // the number of temperatures to analyse
+   int n, t, closest = std::numeric_limits<int>::max();
    std::cin >> n; std::cin.ignore();
-   for (int i = 0; i < n; i++) {
-      int t; // a temperature expressed as an integer ranging from -273 to 5526
+   for (int i = 0; i < n; ++i)
+   {
       std::cin >> t; std::cin.ignore();
       if (std::abs(t) < std::abs(closest) || (std::abs(t) == std::abs(closest) && t > 0))
       {
@@ -15,7 +14,7 @@ int main()
       }
    }
 
-   if (closest == 99999)
+   if (closest == std::numeric_limits<int>::max())
    {
       closest = 0;
    }
