86c9c5effd6130676e2d5186f6004f4181b34496
Marcin Putra <marcin@putra.pro>
2025-09-01 15:41:02 +0200

    Temperatures - Code Golf: C++ 123 chars

--- solo/code_golf/easy/temperatures/temperatures.cpp
+++ solo/code_golf/easy/temperatures/temperatures.cpp
@@ -1,2 +1,2 @@
 #include<iostream>
-main(){int c=999,n,t;std::cin>>n;c=n?c:0;while(n--){std::cin>>t;c=abs(t)<abs(c)||t==abs(c)?t:c;}std::cout<<c;}
+main(){int c=999,n,t;std::cin>>n;c=n?c:0;while(n--){std::cin>>t;c=t*t<c*c||t==abs(c)?t:c;}std::cout<<c;}
