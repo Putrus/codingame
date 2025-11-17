03e7b32794a012e4f0fbdb6b4419ba96dcd8a603
Marcin Putra <marcin@putra.pro>
2025-11-17 11:33:18 +0100

    Mad Pod Racing: Silver League -> Golden League

--- multi/bot-programming/mad-pod-racing/mad_pod_racing.cpp
+++ multi/bot-programming/mad-pod-racing/mad_pod_racing.cpp
@@ -2,17 +2,145 @@
 #include <cmath>
 #include <vector>
 
-std::vector<double> getLineParameters(double x1, double y1, double x2, double y2)
+template <class T>
+class Vector2
 {
-   double a = (y2 - y1) / (x2 - x1);
-   double b = y1 - a * x1;
+public:
+   Vector2();
+   Vector2(T x, T y);
+   Vector2(Vector2 const& other);
+   Vector2(T value);
+   virtual ~Vector2();
+   Vector2 operator+(const Vector2& rhs);
+   Vector2 operator-(const Vector2& rhs);
+   Vector2 operator*(T num);
+   Vector2& operator+=(Vector2 const& rhs);
+   Vector2& operator-=(Vector2 const& rhs);
+   Vector2& operator*=(T num);
+   T distance(const Vector2& other);
+   Vector2 closest(const Vector2& first, const Vector2& second);
+
+   T x;
+   T y;
+};
+
+template <class T>
+Vector2<T>::Vector2() : x((T)0), y((T)0) {}
+
+template <class T>
+Vector2<T>::Vector2(T x, T y) : x(x), y(y) {}
+
+template <class T>
+Vector2<T>::Vector2(const Vector2& other) : x(other.x), y(other.y) {}
+
+template <class T>
+Vector2<T>::Vector2(T value) : x(value), y(value) {}
+
+template <class T>
+Vector2<T>::~Vector2() {}
+
+template <class T>
+Vector2<T> Vector2<T>::operator+(Vector2 const& rhs)
+{
+   return { this->x + rhs.x, this->y + rhs.y };
+}
+
+template <class T>
+Vector2<T> Vector2<T>::operator-(Vector2 const& rhs)
+{
+   return { this->x - rhs.x, this->y - rhs.y };
+}
+
+template <class T>
+Vector2<T> Vector2<T>::operator*(T num)
+{
+   return { this->x * num, this->y * num };
+}
+
+template <class T>
+Vector2<T>& Vector2<T>::operator+=(Vector2 const& rhs)
+{
+   this->x += rhs.x;
+   this->y += rhs.y;
+
+   return *this;
+}
+
+template <class T>
+Vector2<T>& Vector2<T>::operator-=(Vector2 const& rhs)
+{
+   this->x -= rhs.x;
+   this->y -= rhs.y;
+
+   return *this;
+}
+
+template <class T>
+Vector2<T>& Vector2<T>::operator*=(T num)
+{
+   this->x *= num;
+   this->y *= num;
+
+   return *this;
+}
+
+template <class T>
+T Vector2<T>::distance(const Vector2& other)
+{
+   T dx = this->x - other.x;
+   T dy = this->y - other.y;
+
+   return std::sqrt(dx * dx + dy * dy);
+}
+
+template <class T>
+Vector2<T> Vector2<T>::closest(const Vector2& first, const Vector2& second)
+{
+   T dist_first = this->distance(first);
+   T dist_second = this->distance(second);
+
+   return (dist_first < dist_second) ? first : second;
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, Vector2<T> const& vec)
+{
+   os << vec.x << ' ' << vec.y;
+
+   return os;
+}
+
+template <class T>
+std::istream& operator>>(std::istream& is, Vector2<T>& vec)
+{
+   is >> vec.x >> vec.y;
+
+   return is;
+}
+
+template <class T>
+bool operator==(Vector2<T> const& lhs, Vector2<T> const& rhs)
+{
+   return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+template <class T>
+bool operator!=(Vector2<T> const& lhs, Vector2<T> const& rhs)
+{
+   return lhs.x != rhs.x || lhs.y != rhs.y;
+}
+
+std::vector<double> getLineParameters(Vector2<double> p1, Vector2<double> p2)
+{
+   double a = (p2.y - p1.y) / (p2.x - p1.x);
+   double b = p1.y - a * p1.x;
    return { a, b };
 }
 
 int main()
 {
-   std::vector<std::pair<int,int>> checkpoint_list;
-   std::vector<std::pair<int,int>> position_list;
+   std::vector<Vector2<double>> checkpoint_list;
+   std::vector<Vector2<double>> position_list;
 
    int checkpoint_index = 0;
    int frame_index = 0;
@@ -23,66 +151,61 @@ int main()
 
    while (true)
    {
-      int x, y;
-      int next_checkpoint_x, next_checkpoint_y;
+      Vector2<double> pod_position;
+      Vector2<double> next_checkpoint_position;
       int next_checkpoint_distance, next_checkpoint_angle;
 
-      std::cin >> x >> y >> next_checkpoint_x >> next_checkpoint_y >> next_checkpoint_distance >> next_checkpoint_angle;
-      int opponent_x, opponent_y;
+      std::cin >> pod_position >> next_checkpoint_position >> next_checkpoint_distance >> next_checkpoint_angle;
+      Vector2<double> opponent_position;
 
-      std::cin >> opponent_x >> opponent_y;
+      std::cin >> opponent_position;
       double checkpoint_distance = next_checkpoint_distance;
-      double opponent_next_checkpoint_distance = std::sqrt(std::pow(opponent_x - next_checkpoint_x, 2) + std::pow(opponent_y - next_checkpoint_y, 2));
-      double my_opponent_distance = std::sqrt(std::pow(opponent_x - x, 2) + std::pow(opponent_y - y, 2));
+      double opponent_next_checkpoint_distance = next_checkpoint_position.distance(opponent_position);
 
       if (checkpoint_list.empty())
       {
-         checkpoint_list.push_back({next_checkpoint_x, next_checkpoint_y});
+         checkpoint_list.push_back(next_checkpoint_position);
          checkpoint_index++;
       } 
-      else if (checkpoint_list[checkpoint_index - 1].first != next_checkpoint_x ||
-         checkpoint_list[checkpoint_index - 1].second != next_checkpoint_y)
+      else if (checkpoint_list[checkpoint_index - 1] != next_checkpoint_position)
       {
-         checkpoint_list.push_back({next_checkpoint_x, next_checkpoint_y});
+         checkpoint_list.push_back(next_checkpoint_position);
          checkpoint_index++;
       }
 
       if (checkpoint_index >= 2)
       {
-         std::vector<double> new_line_parameters = getLineParameters(checkpoint_list[checkpoint_index - 1].first, checkpoint_list[checkpoint_index - 1].second,
-            checkpoint_list[checkpoint_index - 2].first, checkpoint_list[checkpoint_index - 2].second);
+         std::vector<double> new_line_parameters = getLineParameters(checkpoint_list[checkpoint_index - 1], checkpoint_list[checkpoint_index - 2]);
 
          if (line_parameters != new_line_parameters)
          {
             line_parameters = new_line_parameters;
+            Vector2<double> delta = checkpoint_list[checkpoint_index - 1] - checkpoint_list[checkpoint_index - 2];
 
-            int delta_x = checkpoint_list[checkpoint_index - 1].first - checkpoint_list[checkpoint_index - 2].first;
-            int delta_y = checkpoint_list[checkpoint_index - 1].second - checkpoint_list[checkpoint_index - 2].second;
+            double delta_r = std::sqrt(delta.x * delta.x + delta.y * delta.y);
 
-            double delta_r = std::sqrt(delta_x * delta_x + delta_y * delta_y);
+            double new_delta_x = std::abs(600.0 * delta.x / delta_r);
+            double new_delta_y = std::abs(600.0 * delta.y / delta_r);
 
-            double new_delta_x = std::abs(600.0 * delta_x / delta_r);
-            double new_delta_y = std::abs(600.0 * delta_y / delta_r);
-
-            if (delta_x < 0 && delta_y < 0)
+            if (delta.x < 0 && delta.y < 0)
             {
-               new_destination[0] = checkpoint_list[checkpoint_index - 1].first + new_delta_x;
-               new_destination[1] = checkpoint_list[checkpoint_index - 1].second + new_delta_y;
+               new_destination[0] = checkpoint_list[checkpoint_index - 1].x + new_delta_x;
+               new_destination[1] = checkpoint_list[checkpoint_index - 1].y + new_delta_y;
             } 
-            else if (delta_x < 0 && delta_y > 0)
+            else if (delta.x < 0 && delta.y > 0)
             {
-               new_destination[0] = checkpoint_list[checkpoint_index - 1].first + new_delta_x;
-               new_destination[1] = checkpoint_list[checkpoint_index - 1].second - new_delta_y;
+               new_destination[0] = checkpoint_list[checkpoint_index - 1].x + new_delta_x;
+               new_destination[1] = checkpoint_list[checkpoint_index - 1].y - new_delta_y;
             } 
-            else if (delta_x > 0 && delta_y > 0)
+            else if (delta.x > 0 && delta.y > 0)
             {
-               new_destination[0] = checkpoint_list[checkpoint_index - 1].first - new_delta_x;
-               new_destination[1] = checkpoint_list[checkpoint_index - 1].second - new_delta_y;
+               new_destination[0] = checkpoint_list[checkpoint_index - 1].x - new_delta_x;
+               new_destination[1] = checkpoint_list[checkpoint_index - 1].y - new_delta_y;
             } 
-            else if (delta_x > 0 && delta_y < 0)
+            else if (delta.x > 0 && delta.y < 0)
             {
-               new_destination[0] = checkpoint_list[checkpoint_index - 1].first - new_delta_x;
-               new_destination[1] = checkpoint_list[checkpoint_index - 1].second + new_delta_y;
+               new_destination[0] = checkpoint_list[checkpoint_index - 1].x - new_delta_x;
+               new_destination[1] = checkpoint_list[checkpoint_index - 1].y + new_delta_y;
             }
          }
       }
@@ -98,14 +221,13 @@ int main()
          thrust = 0;
       }
 
-      position_list.push_back({ x, y });
+      position_list.push_back(pod_position);
 
       if (frame_index >= 1)
       {
-         int dx = position_list[frame_index - 1].first - position_list[frame_index].first;
-         int dy = position_list[frame_index - 1].second - position_list[frame_index].second;
+         Vector2<double> delta = position_list[frame_index - 1] - position_list[frame_index];
 
-         if (std::abs(dx) < 350 && std::abs(dy) < 350)
+         if (std::abs(delta.x) < 350 && std::abs(delta.y) < 350)
          {
             thrust = 100;
          }
@@ -117,7 +239,7 @@ int main()
          checkpoint_distance < 1200) 
       {
          boost_used = 1;
-         std::cout << opponent_x << " " << opponent_y << " BOOST\n";
+         std::cout << opponent_position.x << " " << opponent_position.y << " BOOST\n";
       }
       else if (checkpoint_index >= 2)
       {
@@ -125,7 +247,7 @@ int main()
       }
       else
       {
-         std::cout << next_checkpoint_x << " " << next_checkpoint_y << " " << thrust << std::endl;
+         std::cout << next_checkpoint_position.x << " " << next_checkpoint_position.y << " " << thrust << std::endl;
       }
 
       frame_index++;
